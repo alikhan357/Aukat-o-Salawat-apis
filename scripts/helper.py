5ae43917a253ffb97a5a387d69aa53689939a466
alikhan357 <ali@capelinsolutions.com>
2023-06-07 14:48:40 +0500

    script updated

--- scripts/helper.py
+++ scripts/helper.py
@@ -2,6 +2,7 @@ import os
 import requests
 import pygame
 import time
+from datetime import datetime, timedelta
 
 
 REMINDERS_API_URL = "http://localhost:5000/api/v1/reminder/pi/reminders/{}"
@@ -43,3 +44,20 @@ def getReminders(email):
         print("FAILED WITH STATUS CODE {}".format(response.status_code))
 
     return output
+
+
+def add_subtract_time(time_str, minutes):
+    # Convert time string to a datetime object
+    time_obj = datetime.strptime(time_str, "%H:%M")
+
+    # Create a timedelta object for the given number of minutes
+    time_delta = timedelta(minutes=minutes)
+
+    # Perform addition/subtraction on the time object
+    result_time = time_obj + time_delta
+
+    # Format the resulting time as a string
+    result_str = result_time.strftime("%H:%M")
+
+    return result_str
+
--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
 import schedule
 import time
-from helper import getReminders, play
+from helper import getReminders, play,add_subtract_time
 from pytz import timezone
 
 # Dictionary to store reminder times for each namaz
@@ -8,7 +8,7 @@ reminders_times = {}
 
 # User email and default timezone
 email = "aliammarkhanbitw@gmail.com"
-default_timezone = "Asia/Calcutta"
+default_timezone = "Asia/Karachi"
 
 # Path format for audio files
 audio_path = "audio/{}"
@@ -26,7 +26,7 @@ def updateReminders():
 
     if len(reminders) > 0:
         for reminder in reminders:
-            time = reminder["time"].split(" ")[0]
+            time = add_subtract_time(reminder["time"].split(" ")[0],reminder["adjustedTime"])
             # Check if reminder needs to be updated
             if not (reminder["namaz"] in reminders_times and reminders_times[reminder["namaz"]] == time):
                 # Clear any existing reminder for the namaz
